8e829ecde64f666052bfc5599116836c10b84a1b
prithvihttp <prithviraj6868@gmail.com>
2025-06-26 13:51:45 +0530

    setup done

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import { connectDB } from "./config/dbConnect.js"
+import app from "./app.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = 8000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+    
+})
